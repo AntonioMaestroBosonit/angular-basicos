966f5b8db9ce7646b0866d15a815ee7f6798b55f
AntonioMaestroBosonit <antonio.maestro@bosonit.com>
2022-07-25 12:58:47 +0200

    leccion 5

---
+++ src/app/dbz/agregar/agregar.component.ts
@@ -0,0 +1,37 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
+
+@Component({
+  selector: 'app-agregar',
+  templateUrl: './agregar.component.html',
+})
+export class AgregarComponent {
+
+  @Input() nuevo: Personaje = {
+    nombre: '',
+    poder: 0,
+  };
+
+  // @Output() onNuevoPersonaje: EventEmitter<Personaje> = new EventEmitter();
+
+  cambiarNombre(event: any) {
+    console.log(event.target.value);
+  }
+
+  constructor(private DbzService: DbzService) {}
+
+  agregar() {
+    if (this.nuevo.nombre.trim().length === 0) {
+      return;
+    }
+
+    // this.onNuevoPersonaje.emit(this.nuevo);
+    this.DbzService.agregarPersonaje(this.nuevo);
+
+    this.nuevo = {
+      nombre: '',
+      poder: 0,
+    };
+  }
+}
